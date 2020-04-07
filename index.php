f6b134a9250623833e1cc267bbf0bafe7d6035e7
zzibuyu <husterfan@qq.com>
2020-04-07 20:01:48 +0800

    第一次提交

---
+++ index.php
@@ -0,0 +1,182 @@
+<html>
+<head>
+    <meta charset="utf-8">
+	<meta name="keywords" content="bilibili,直播,live,哔哩哔哩live直链,B站,哔哩哔哩">
+    <meta name="description" content="哔哩哔哩live直链获取">
+	<meta itemprop="name" content="bilibili live 直链获取">
+	<meta itemprop="description" content="bilibili live 直链获取">
+	<meta itemprop="image" content="https://q.qlogo.cn/g?b=qq&nk=800059038&s=100">
+	<meta name="referrer" content="no-referrer">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>bilibili live 直链获取</title>
+    <style>
+        html,
+        body {
+            height: 100%;
+        }
+        
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+        }
+        
+        .container {
+            text-align: center;
+            display: block;
+            position: relative;
+            top: 100px;
+            vertical-align: middle;
+			position: relative;
+			min-height: 100%;
+			padding-bottom: 50px;
+        }
+		@media screen and (max-width: 375px){
+			.container {
+				top: 20px;
+			}
+		}
+        
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+        
+        .title {
+            font-size: 66px;
+        }
+		
+		hr{
+			border: 0.5px dashed silver;
+			width: 50%;
+		}
+		
+		a {
+			text-decoration: none;
+			color: black;
+		}
+		
+		a:hover{ 
+			color: #00A1D6;
+		}
+		
+	</style>
+</head>
+<body>
+<div>
+    <div class="container">
+        <div class="content">
+            <div class="title">bilibili live 直链获取</div>
+			<span>（1.若无法播放，换用其它地址</span>
+			<span>2.只能获取直播间直链，不能用于投稿视频</span>
+			<br>
+			<span>3.b站视频解析可用 https://www.parsevideo.net </span>
+			<span>4.<a href="#" target="_blank">源码</a>）</span>
+			
+        </div>
+        <h3>请在下方输入房间号</h3>
+		<div>
+			<form action="/index.php">
+				<input type="text" id='room_id' name="room_id" pattern="\d*" title="只能输入数字" placeholder="请输入房间号" maxlength="25" value="" required>
+				<label>清晰度：</label>
+				<input type="radio" name="quality" value="10000" checked>原画
+				<input type="radio" name="quality" value="150">高清
+				<input type="checkbox" name="sina" value="1">短链	
+				<br>
+				<br>
+				<button id="submit">获取</button>
+			</form>
+		</div>
+        <?php
+		if (isset($_GET['room_id'], $_GET['quality'])){
+			$room_id = trim($_GET['room_id']);  //清理空格
+			$room_id = strip_tags($room_id);   //过滤html标签  
+			$room_id = htmlspecialchars($room_id);   //将字符内容转化为html实体  
+			$room_id = addslashes($room_id);  //防止SQL注入
+			
+			$quality = trim($_GET['quality']);
+			$quality = strip_tags($quality);   //过滤html标签  
+			$quality = htmlspecialchars($quality);   //将字符内容转化为html实体  
+			$quality = addslashes($quality);  //防止SQL注入
+			
+			if (isset($_GET['sina'])){
+				$sina = trim($_GET['sina']);  //清理空格
+				$sina = strip_tags($sina);   //过滤html标签  
+				$sina = htmlspecialchars($sina);   //将字符内容转化为html实体  
+				$sina = addslashes($sina);  //防止SQL注入
+			}
+
+			/* echo $room_id; */
+			echo "<script>document.getElementById('room_id').value = $room_id </script>";
+			
+			if($room_id != "" && $quality == "10000" || $quality == "150"){
+				preg_match('/\d+/', $room_id, $matches);
+				if (count($matches)){
+					$live_data = live_status($matches[0]);
+					if (array_key_exists("live_status", $live_data) && $live_data['live_status'] == 1){
+						echo '<h3>房间号：' . $live_data["roomid"] . '</h3>';
+						echo '<h3>up：' . $live_data["uname"] . '</h3>';
+						echo '<h3>标题：' . $live_data["title"] . '</h3>';
+						echo '<br>';
+						$live_info = geturl('https://api.live.bilibili.com/xlive/web-room/v1/playUrl/playUrl?cid='.$matches[0].'&qn='.$quality.'&platform=web');
+						$live_urls = $live_info['data']['durl'];
+						$durl_length = count($live_urls);
+						for($i=0; $i<$durl_length; $i++){
+							if (isset($sina) && $sina === "1"){
+								echo get_turl($live_urls[$i]['url']);
+								echo "<hr/>";
+							}else{
+								echo $live_urls[$i]['url'];
+								echo "<hr/>";
+							}
+						}
+					}else{
+						echo 'up主还未开播';
+					}
+				}
+			}
+		}
+		?>
+	</div>
+</div>
+</body>
+<?php
+# 生成短链
+function get_turl($url){
+	$t_url = 'https://api.jike.info/t_cn/' . urlencode($url);
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $t_url);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$output = curl_exec($ch);
+	curl_close($ch);
+	return $output;
+}
+
+# 当前开播状态
+function live_status($room_id){
+	error_reporting(E_ALL ^ E_NOTICE);   # 关闭Notice提示
+	$live_room = geturl('https://api.live.bilibili.com/room/v1/RoomStatic/get_room_static_info?room_id='.$room_id);
+	return $live_room['data']; # 轮播的状态码为2，也没法看
+}
+
+function geturl($url){
+	$headerArray =array("Content-type:application/json;","Accept:application/json");
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch,CURLOPT_HTTPHEADER,$headerArray);
+	$output = curl_exec($ch);
+	curl_close($ch);
+	$output = json_decode($output,true);
+	return $output;
+}
+
+// get_turl('https://www.runoob.com/js/js-tutorial.html')
+
+?>
